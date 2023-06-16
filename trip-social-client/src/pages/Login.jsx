ccac76b3cddd4434698576782b56f7685e0bb544
Malik <willis.malik.r@gmail.com>
2023-06-16 02:02:00 -0500

    first login in, but no token response

--- trip-social-client/src/pages/Login.jsx
+++ trip-social-client/src/pages/Login.jsx
@@ -1,7 +1,50 @@
+import {useState} from "react";
+
+
 export default function Login(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function submitHandler(e) {
+        e.preventDefault();
+        const user = {
+            username,
+            password,
+        };
+
+        const auth = btoa(`${username}:${password}`);
+        await fetch(`http://localhost:8080/api/auth/token`, {
+            method: "POST",
+            headers: {
+                // "Content-type": "application/json",
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': `Basic ${auth}`
+            },
+        });
+
+
+        setUsername("");
+        setPassword("");
+    }
     return(
         <>
             <h1>Login</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    type = "text"
+                    placeholder = "Username"
+                    value = {username}
+                    onChange = {(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type = "text"
+                    placeholder = "Password"
+                    value = {password}
+                    onChange = {(e) => setPassword(e.target.value)}
+                />
+                <button type = "Submit">Submit</button>
+            </form>
         </>
     )
 }
