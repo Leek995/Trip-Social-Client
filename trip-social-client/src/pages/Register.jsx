8ee8b4a9738bc39d3a763ce0840a99cfe3275fca
Malik <willis.malik.r@gmail.com>
2023-06-15 23:39:27 -0500

    able to register user from client

---
+++ trip-social-client/src/pages/Register.jsx
@@ -0,0 +1,42 @@
+import {useState} from "react";
+export default function Register(){
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    async function submitHandler(e){
+        e.preventDefault();
+        const user = {
+            username,
+            password
+        };
+
+        await fetch(`http://localhost:8080/register`, {
+            method: "POST",
+            headers: {"Content-type" : "application/json"},
+            body: JSON.stringify(user)
+        });
+
+        setUsername("");
+        setPassword("");
+    }
+    return(
+        <>
+            <h1>Register Page</h1>
+            <form onSubmit={submitHandler}>
+                <input
+                    type = "text"
+                    placeholder = "Username"
+                    value = {username}
+                    onChange = {(e) => setUsername(e.target.value)}
+                />
+
+                <input
+                    type = "text"
+                    placeholder = "Password"
+                    value = {password}
+                    onChange = {(e) => setPassword(e.target.value)}
+                />
+                <button type = "Submit">Submit</button>
+            </form>
+        </>
+    )
+}
